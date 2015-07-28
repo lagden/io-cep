0e08126b55d150d118527b7cf9c807c8cb9bae47
Thiago Lagden <lagden@gmail.com>
2015-07-28 05:38:17 -0300

    replace request by got

--- es6/index.js
+++ es6/index.js
@@ -1,8 +1,44 @@
 'use strict';
 
-import request from 'request';
-import iconv   from 'iconv-lite';
+import got from 'got';
+import iconv from 'iconv-lite';
 import {parse, cleanup} from './lib/utility';
+
+function sucesso(cep, res) {
+  let data;
+  if (res.statusCode === 200) {
+    data = parse(iconv.decode(res._buffer, 'iso-8859-1'));
+    if (data) {
+      data.success = true;
+      data = cleanup(data, 'logradouro');
+      data = cleanup(data, 'endere\u00E7o');
+      if (data.hasOwnProperty('endere\u00E7o')) {
+        data.logradouro = data['endere\u00E7o'];
+      }
+    } else {
+      data = {
+        success: false,
+        message: 'CEP not found or parse error',
+        cep: cep,
+      };
+    }
+  } else {
+    data = {
+      success: false,
+      message: `Status code is ${res.statusCode}`,
+      cep: cep,
+    };
+  }
+  return Promise.resolve(data);
+}
+
+function falha(cep, err) {
+  return Promise.resolve({
+    success: false,
+    message: err,
+    cep: cep,
+  });
+}
 
 /**
  * Consulta.
@@ -10,54 +46,25 @@ import {parse, cleanup} from './lib/utility';
  * @param {string} cep - Zip code of the location.
  */
 function consulta(cep) {
-  return new Promise((resolve, reject) => {
-    if (typeof cep !== 'string') {
-      throw new Error('Must be a string');
-    }
-    if (/^(\d{5})\-?(\d{3})$/.test(cep) === false) {
-      throw new Error('Invalid format');
-    }
-    let formData = {
-      form: {
-        cepEntrada: cep,
-        tipoCep: '',
-        cepTemp: '',
-        metodo: 'buscarCep',
-      },
-      encoding: null,
-      timeout: 5000,
-    };
-    request.post(
-      'http://m.correios.com.br/movel/buscaCepConfirma.do',
-      formData,
-      (err, res, body) => {
-        if (!err && res.statusCode === 200) {
-          let data = parse(iconv.decode(body, 'ISO-8859-1'));
-          if (data) {
-            data.success = true;
-            data = cleanup(data, 'logradouro');
-            data = cleanup(data, 'endere\u00E7o');
-            if (data.hasOwnProperty('endere\u00E7o')) {
-              data.logradouro = data['endere\u00E7o'];
-            }
-            resolve(data);
-          } else {
-            resolve({
-              success: false,
-              message: 'CEP not found or parse error',
-              cep: cep,
-            });
-          }
-        } else {
-          resolve({
-            success: false,
-            message: 'Request timemout',
-            cep: cep,
-          });
-        }
-      }
-    );
-  });
+  if (typeof cep !== 'string') {
+    Promise.reject('Must be a string');
+  }
+  if (/^(\d{5})\-?(\d{3})$/.test(cep) === false) {
+    Promise.reject('Invalid format');
+  }
+  let formData = {
+    body: {
+      cepEntrada: cep,
+      tipoCep: '',
+      cepTemp: '',
+      metodo: 'buscarCep',
+    },
+    timeout: 10000,
+  };
+  return got
+    .post('http://m.correios.com.br/movel/buscaCepConfirma.do', formData)
+    .then(sucesso.bind(sucesso, cep))
+    .catch(falha.bind(falha, cep));
 }
 
 export default consulta;
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, '__esModule', {
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _request = require('request');
+var _got = require('got');
 
-var _request2 = _interopRequireDefault(_request);
+var _got2 = _interopRequireDefault(_got);
 
 var _iconvLite = require('iconv-lite');
 
@@ -16,51 +16,59 @@ var _libUtility = require('./lib/utility');
 
 'use strict';
 
-function consulta(cep) {
-  return new Promise(function (resolve, reject) {
-    if (typeof cep !== 'string') {
-      throw new Error('Must be a string');
-    }
-    if (/^(\d{5})\-?(\d{3})$/.test(cep) === false) {
-      throw new Error('Invalid format');
-    }
-    var formData = {
-      form: {
-        cepEntrada: cep,
-        tipoCep: '',
-        cepTemp: '',
-        metodo: 'buscarCep'
-      },
-      encoding: null,
-      timeout: 5000
-    };
-    _request2.default.post('http://m.correios.com.br/movel/buscaCepConfirma.do', formData, function (err, res, body) {
-      if (!err && res.statusCode === 200) {
-        var data = (0, _libUtility.parse)(_iconvLite2.default.decode(body, 'ISO-8859-1'));
-        if (data) {
-          data.success = true;
-          data = (0, _libUtility.cleanup)(data, 'logradouro');
-          data = (0, _libUtility.cleanup)(data, 'endere\u00E7o');
-          if (data.hasOwnProperty('endere\u00E7o')) {
-            data.logradouro = data['endere\u00E7o'];
-          }
-          resolve(data);
-        } else {
-          resolve({
-            success: false,
-            message: 'CEP not found or parse error',
-            cep: cep
-          });
-        }
-      } else {
-        resolve({
-          success: false,
-          message: 'Request timemout',
-          cep: cep
-        });
+function sucesso(cep, res) {
+  var data = undefined;
+  if (res.statusCode === 200) {
+    data = (0, _libUtility.parse)(_iconvLite2.default.decode(res._buffer, 'iso-8859-1'));
+    if (data) {
+      data.success = true;
+      data = (0, _libUtility.cleanup)(data, 'logradouro');
+      data = (0, _libUtility.cleanup)(data, 'endere\u00E7o');
+      if (data.hasOwnProperty('endere\u00E7o')) {
+        data.logradouro = data['endere\u00E7o'];
       }
-    });
+    } else {
+      data = {
+        success: false,
+        message: 'CEP not found or parse error',
+        cep: cep
+      };
+    }
+  } else {
+    data = {
+      success: false,
+      message: 'Status code is ' + res.statusCode,
+      cep: cep
+    };
+  }
+  return Promise.resolve(data);
+}
+
+function falha(cep, err) {
+  return Promise.resolve({
+    success: false,
+    message: err,
+    cep: cep
   });
+}
+
+function consulta(cep) {
+  if (typeof cep !== 'string') {
+    Promise.reject('Must be a string');
+  }
+  if (/^(\d{5})\-?(\d{3})$/.test(cep) === false) {
+    Promise.reject('Invalid format');
+  }
+  var formData = {
+    body: {
+      cepEntrada: cep,
+      tipoCep: '',
+      cepTemp: '',
+      metodo: 'buscarCep'
+    },
+    timeout: 10000
+  };
+  return _got2.default.post('http://m.correios.com.br/movel/buscaCepConfirma.do', formData).then(sucesso.bind(sucesso, cep)).catch(falha.bind(falha, cep));
 }
 
 exports.default = consulta;
